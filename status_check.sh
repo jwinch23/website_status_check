64c6536c61004a9e37d9710a5928d0b32f5afcc1
Josh Winchester <jwinchester23@gmail.com>
2023-04-15 21:24:07 -0500

    Update logic to handle .com without www.
    Update the parsing logic. If a user passes example.com instead of www.example.com or example the script will know how to properly handle this edge case.

--- status_check.sh
+++ status_check.sh
@@ -9,9 +9,8 @@ fi
 domain=$1
 if [[ "$domain" != www.* && "$domain" != *.* ]]; then
     domain="www.$domain.com"
-    # if [[ "$domain" = *.com ]]; then
-    #     domain="www.$domain"
-    #     echo "foo"
+elif [[ "$domain" != www.* ]]; then
+    domain="www.$domain"
 fi
 
 status_code=$(curl -s -o /dev/null -w "%{http_code}" $domain)
