7baa0c9a552d4e0cd0cca83668599fadff862608
Josh Winchester <jwinchester23@gmail.com>
2023-04-15 21:20:39 -0500

    Add status_check.sh
    A shell script to get the current status code for the website provided by the parameters.

---
+++ status_check.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Please provide a parameter"
+    echo "Example: example or www.example.com"
+    exit 1
+fi
+
+domain=$1
+if [[ "$domain" != www.* && "$domain" != *.* ]]; then
+    domain="www.$domain.com"
+    # if [[ "$domain" = *.com ]]; then
+    #     domain="www.$domain"
+    #     echo "foo"
+fi
+
+status_code=$(curl -s -o /dev/null -w "%{http_code}" $domain)
+
+if [ $status_code -eq 200 ]; then
+    echo "$(date) - Up - $status_code"
+else
+    echo "$(date) - Down - $status_code"
+fi
